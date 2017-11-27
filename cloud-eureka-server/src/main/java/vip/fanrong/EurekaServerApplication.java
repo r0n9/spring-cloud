b1fbe411960b6be0e49219f336641e1bb90061fc
r0n9 <fanrong330@gmail.com>
2017-11-27 23:23:32 +0800

    spring-boot-maven-plugin

--- cloud-eureka-server/src/main/java/vip/fanrong/EurekaServerApplication.java
+++ cloud-eureka-server/src/main/java/vip/fanrong/EurekaServerApplication.java
@@ -2,8 +2,9 @@ package vip.fanrong;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
