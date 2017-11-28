af2f15161715257d7521965c3c269530a8a5bd71
r0n9 <fanrong330@gmail.com>
2017-11-28 21:25:13 +0800

    update script

--- script/build.sh
+++ script/build.sh
@@ -16,10 +16,12 @@ ps -ef|grep java|grep rong|grep cloud-eureka-consumergrep -v grep|cut -c 9-15|xa
 
 
 cd $base_path
-java -jar cloud-eureka-server/target/cloud-eureka-server-1.0-SNAPSHOT.jar >~/logs/cloud-eureka-server.out 2>&1 &
+java -jar cloud-eureka-server/target/cloud-eureka-server-1.0-SNAPSHOT.jar >~/logs/cloud-eureka-server.out 2>&1
 sleep 1
-java -jar cloud-api-gateway/target/cloud-api-gateway-1.0-SNAPSHOT.jar >~/logs/cloud-api-gateway.out 2>&1 &
+java -jar cloud-api-gateway/target/cloud-api-gateway-1.0-SNAPSHOT.jar >~/logs/cloud-api-gateway.out 2>&1
 sleep 1
-java -jar client-kds-crawler/target/client-kds-crawler-1.0-SNAPSHOT.jar >~/logs/client-kds-crawler.out 2>&1 &
+java -jar client-kds-crawler/target/client-kds-crawler-1.0-SNAPSHOT.jar >~/logs/client-kds-crawler.out 2>&1
 sleep 1
-java -jar cloud-eureka-consumer/target/cloud-eureka-consumer-1.0-SNAPSHOT.jar >~/logs/cloud-eureka-consumer.out 2>&1 &
+java -jar cloud-eureka-consumer/target/cloud-eureka-consumer-1.0-SNAPSHOT.jar >~/logs/cloud-eureka-consumer.out 2>&1
+
+
