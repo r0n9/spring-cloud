47f6c0d4afd387e0fc1aee03cf8f7f10b55fa79c
r0n9 <fanrong330@gmail.com>
2017-12-04 20:55:06 +0800

    proxy crawler

--- script/build.sh
+++ script/build.sh
@@ -2,15 +2,6 @@
 
 base_path=$(cd `dirname $0`; cd ..; pwd)
 
-# maven build
 cd $base_path
 
-scp client-web-crawler/target/*.jar rong@s4.fanrong.vip:/home/rong/cloud/client-web-crawler
-scp client-web-crawler/target/classes/application.properties rong@s4.fanrong.vip:/home/rong/cloud/client-web-crawler/config
-
-# 停止client-web-crawler
-ssh rong@s4.fanrong.vip 'ps -ef|grep java|grep rong|grep client-web-crawler|grep -v grep|cut -c 9-15|xargs kill -9'
-
-# 启动client-web-crawler
-ssh rong@s4.fanrong.vip 'cd /home/rong/cloud/client-web-crawler; nohup java -jar client-web-crawler-1.0-SNAPSHOT.jar > ~/logs/client-web-crawler.out 2>&1 &'
-
+mvn clean install -DskipTests -Pprod
--- script/run.sh
+++ script/run.sh
@@ -1,85 +1,15 @@
 ﻿#!/bin/bash
 
-if [ ! -z $JAVA_HOME ]; then
-    echo "Use \$JAVA_HOME"
-elif [ -n `which java` ]; then
-    echo "Use system java"
-    echo `java -version`
-else
-    echo "java does not exist"
-    exit 1
-fi
-
 base_path=$(cd `dirname $0`; cd ..; pwd)
-bin_path=$base_path/bin
-jar_file=$bin_path/xxxxxxxxxxxxxxxxxxxxxxx.jar
-conf_path=$base_path/conf
-prop_file=$conf_path/application.properties
-log_path=$base_path/logs
-log_file=$log_path/xxxxxxxxxxxxxxxxxxxxxxx.log
-pid_path=$log_path/pid
-pid_file=$pid_path/xxxxxxxxxxxxxxxxxxxxxxx.pid
-stdout_file=$log_path/std.out
 
-mkdir -p $pid_path >/dev/null 2>&1
+cd $base_path
 
-check_start() {
-	if [ -f $pid_file ]; then
-		pid=`head -n1 $pid_file`
-		if kill -0 $pid >/dev/null 2>&1; then
-			echo "xxxxxxxxxxxxxxxxxxxxxxx service has already started. Stop it first."
-			exit 1
-		fi
-	fi
-}
+scp client-web-crawler/target/*.jar rong@s4.fanrong.vip:/home/rong/cloud/client-web-crawler
+scp client-web-crawler/target/classes/application.properties rong@s4.fanrong.vip:/home/rong/cloud/client-web-crawler/config
 
-check_stop() {
-    if [ -f $pid_file ]; then
-		pid=`head -n1 $pid_file`
-		if kill -0 $pid >/dev/null 2>&1; then
-			echo "xxxxxxxxxxxxxxxxxxxxxxx service is running as process $pid."
-		fi
-	else
-		echo "No pid file detected. Maybe there is no service running."
-		exit 1
-	fi
-}
+# 停止client-web-crawler
+ssh rong@s4.fanrong.vip 'ps -ef|grep java|grep rong|grep client-web-crawler|grep -v grep|cut -c 9-15|xargs kill -9'
 
-do_start() {
-    java -Dext.resources=$conf_path -Dspring.config.location=$prop_file -Dlogging.file=$log_file -jar $jar_file >$stdout_file 2>&1 &
-    echo $! > $pid_file
-    echo "xxxxxxxxxxxxxxxxxxxxxxx service started. Process id is `head -n1 $pid_file`."
-}
+# 启动client-web-crawler
+ssh rong@s4.fanrong.vip 'cd /home/rong/cloud/client-web-crawler; nohup java -jar client-web-crawler-1.0-SNAPSHOT.jar > ~/logs/client-web-crawler.out 2>&1 &'
 
-do_stop() {
-	kill $pid > /dev/null 2>&1
-	echo -n "Now stopping"
-	while kill -0 $pid > /dev/null 2>&1;
-	do
-		echo -n "."
-		sleep 1;
-	done
-	echo ""
-	rm -f $pid_file
-}
-
-case $1 in
-    "start")
-        check_start
-        do_start
-    ;;
-    "stop")
-        check_stop
-        do_stop
-    ;;
-    "restart")
-        check_stop
-        do_stop
-        check_start
-        do_start
-    ;;
-    *)
-        echo "Usage $0 [start|stop|restart]"
-        exit 1
-    ;;
-esac
