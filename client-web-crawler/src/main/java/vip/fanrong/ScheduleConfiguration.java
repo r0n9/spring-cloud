12d03e1d0b954a89bfb2dbce4d7944c0b4912515
r0n9 <fanrong330@gmail.com>
2017-12-04 20:57:23 +0800

    proxy crawler

--- client-web-crawler/src/main/java/vip/fanrong/ScheduleConfiguration.java
+++ client-web-crawler/src/main/java/vip/fanrong/ScheduleConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import vip.fanrong.service.ProxyCrawlerService;
 import vip.fanrong.service.ZmzCrawlerService;
 
 /**
@@ -16,10 +17,18 @@ public class ScheduleConfiguration implements SchedulingConfigurer {
     @Autowired
     ZmzCrawlerService zmzCrawlerService;
 
+    @Autowired
+    ProxyCrawlerService proxyCrawlerService;
+
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
         scheduledTaskRegistrar.addCronTask(() -> {
             zmzCrawlerService.loadZmzResourceTops();
         }, "0 0 6,18 * * ?");
+
+
+        scheduledTaskRegistrar.addCronTask(() -> {
+            proxyCrawlerService.loadSocksProxyConfigsFromGatherproxy(null, null);
+        }, "0 0 * * * ?");
     }
 }
