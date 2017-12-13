724177e34503edebcf4d63eb8b9f9692bc0788e5
r0n9 <fanrong330@gmail.com>
2017-12-13 10:16:56 +0800

    Default TimeZone

--- client-web-crawler/src/main/java/vip/fanrong/ClientWebCrawlerApplication.java
+++ client-web-crawler/src/main/java/vip/fanrong/ClientWebCrawlerApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.TimeZone;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ClientWebCrawlerApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(ClientWebCrawlerApplication.class, args);
     }
 }
