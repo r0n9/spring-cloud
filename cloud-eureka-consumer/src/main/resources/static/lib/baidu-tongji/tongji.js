f84cfbb807c1baf5429214b3599adea0065a4497
r0n9 <fanrong330@gmail.com>
2018-01-17 20:49:35 +0800

    baidu tongji

---
+++ cloud-eureka-consumer/src/main/resources/static/lib/baidu-tongji/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?70da833ac20659b0e1bbd2a337cb82e8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
